c35da0c17c457a7aab0d6905479b5e408efdc813
mandip sanger <mandipsanger1@gmail.com>
2026-09-08 17:52:07 +0100

    implement bmi.js

--- Sprint-3/3-mandatory-implement/1-bmi.js
+++ Sprint-3/3-mandatory-implement/1-bmi.js
@@ -6,14 +6,17 @@
 
 // squaring your height: 1.73 x 1.73 = 2.99
 // dividing 70 by 2.99 = 23.41
-// Your result will be displayed to 1 decimal place, for example '23.4'.
+// Your result will be displayed to 1 decimal place, for example 23.4.
 
 // You will need to implement a function that calculates the BMI of someone based off their weight and height
 
 // Given someone's weight in kg and height in metres
 // Then when we call this function with the weight and height
-// It should return a string of their Body Mass Index to 1 decimal place
+// It should return their Body Mass Index to 1 decimal place
 
 function calculateBMI(weight, height) {
-  // return the BMI of someone based off their weight and height
+  return (weight / (height * height)).toFixed(1);
 }
+console.log(calculateBMI(90, 1.7));
+// return the BMI of someone based off their weight and height
+// return the BMI of someone based off their weight and height
