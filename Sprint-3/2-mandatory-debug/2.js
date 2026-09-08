038f2d72dcbc5cebcd48903e13b9ccc4de01ab6f
mandip sanger <mandipsanger1@gmail.com>
2026-09-08 17:51:04 +0100

    debug 2.js

--- Sprint-3/2-mandatory-debug/2.js
+++ Sprint-3/2-mandatory-debug/2.js
@@ -1,24 +1,36 @@
-// Predict and explain first...
+// Predict and explain first...const num is declared outside the function, so it is not accessible inside the function. This will cause a ReferenceError.
+// To fix this, we should pass the number as a parameter to the function and use that parameter inside the function.
 
 // Predict the output of the following code:
-// =============> Write your prediction here
+// =============> Write your prediction here 3,3,3
 
-const num = 103;
+//const num = 103;
 
-function getLastDigit() {
-  return num.toString().slice(-1);
-}
+//function getLastDigit() {
+//return num.toString().slice(-1);
+//}
 
-console.log(`The last digit of 42 is ${getLastDigit(42)}`);
-console.log(`The last digit of 105 is ${getLastDigit(105)}`);
-console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+//console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+//console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+//console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // Now run the code and compare the output to your prediction
-// =============> write the output here
+// =============> 2,5,6
 // Explain why the output is the way it is
-// =============> write your explanation here
+// =============> the output is the way function is called with an argument, so the function is able to access the value of the argument passed to it and return the last digit of that number.
+//  The function is not using the variable num declared outside the function, so it does not cause a ReferenceError.
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+
+const num = 103;
+
+function getLastDigit(n) {
+  return n.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
