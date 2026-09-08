0be3a97d88536a26b4c02f5d18d15f34c369a226
mandip sanger <mandipsanger1@gmail.com>
2026-09-08 17:07:52 +0100

    fixied bot issues so adding file back

--- Sprint-2/1-key-exercises/1-count.js
+++ Sprint-2/1-key-exercises/1-count.js
@@ -4,3 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// line 3 is adding plus 1 to  the original value of count for example if count is 2 line 3 result will be 2+1=3, and store it back to count
