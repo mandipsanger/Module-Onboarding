d449428f3e3bda6118febcacf73089e8334b8782
mandip sanger <mandipsanger1@gmail.com>
2026-09-08 17:50:26 +0100

    manadatory debug  1.js

--- Sprint-3/2-mandatory-debug/1.js
+++ Sprint-3/2-mandatory-debug/1.js
@@ -1,13 +1,19 @@
-// Predict and explain first...
-//  =============> write your prediction here
+// Predict and explain first... function doesn't return anything, so the result of multiplying 10 and 32 is undefined.
+// To fix this, we should add a return statement to the function to return the result of multiplying a and b.
+//  =============> write your prediction here 'The sum of 10 and 32 is undefined'
 
-function sum(a, b) {
-  return;
-  a + b;
-}
+//function sum(a, b) {
+//return;
+//a + b;
+//}
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+//console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
