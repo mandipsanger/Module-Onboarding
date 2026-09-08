3f94fb8dda4dba6b50eb4b2ad1310586c834c6ac
mandip sanger <mandipsanger1@gmail.com>
2026-09-08 17:48:08 +0100

    key errot2

--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -1,20 +1,21 @@
-
-// Predict and explain first BEFORE you run any code...
+// SyntaxError: Unexpected token '3'
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
 
-function square(3) {
-    return num * num;
-}
+//function square(3) {
+//    return num * num;
+//}
 
-// =============> write the error message here
-
+// =============> write the error message here SyntaxError: Unexpected token (1:16)
 // =============> explain this error message here
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+  return num * num;
+}
 
-
+console.log(square(3));
